adc05569c9011924545eca991ba1246af05158e8
difa <99042935+difanys@users.noreply.github.com>
2023-08-15 08:28:19 +0300

    Update mtk.lua
    Переписан код с библиотеки Forms, на стандартные библиотеки

--- mtk.lua
+++ mtk.lua
@@ -1,78 +1,85 @@
---МТК ЧАЭС
-require("component").gpu.setResolution(120,60)
-local c = require("component")
-local rsio = c.proxy("8ab1384e-a8a3-448c-9db5-7ea28d560e17") --Дебаг рсио
-forms=require("forms")
-forms.ignoreAll()
+--MTK by NuclearBuilders
 
-function Form1onCreate(self)
-forms.stop()
+local computer = require("computer")
+local c = require("component")
+local fs = require("filesystem")
+local term = require("term")
+local rsio = c.proxy("8ab1384e-a8a3-448c-9db5-7ea28d560e17") --Адрес рсио который будет управлять нашим МТК
+local gpu = c.gpu
+
+gpu.setResolution(120, 60) --Ставим разрешение МТК
+
+gpu.setBackground(0x1f1f1f) --Устанавливаем цвет фона 
+gpu.setForeground(0xFFFFFF)
+
+gpu.fill(1, 1, 120, 60, " ")
+
+--ЗаставОЧКА
+term.setCursor(50, 30)
+term.write("МТК ВИУР 2023 г.")
+term.setCursor(50, 32)
+term.write("By NuclearBuilders")
+computer.beep(200)
+computer.beep(300)
+computer.beep(500)
+computer.beep(800)
+os.sleep(2)
+term.clear()
+
+--Код для вычисления реального времени
+local TIME_ZONE = 4 --Таймзона (по умолчанию 0)
+local t_correction = TIME_ZONE * 3600 
+local function getTimeHost()
+    local file = fs.open('unix.tmp', 'w')
+    file:write('')
+    file:close()
+    local lastmod = tonumber(string.sub(fs.lastModified('unix.tmp'), 1, -4)) + t_correction
+    return lastmod
 end
 
-Form1=forms.addForm()
-Form1.color=5197647
-Form1.border=1
-
-exit=Form1:addButton(116,59,"Выход")
-exit.W=2
-
---Режимы МТК
-Label1=Form1:addLabel(20,3,"Tгаз")
-Label1.W=4
-
-Label2=Form1:addLabel(29,3,"Nтк")
-Label2.W=3
-
-Label3=Form1:addLabel(38,3,"Тгр")
-Label3.W=3
-
-Label4=Form1:addLabel(47,3,"Pтк")
-Label4.W=3
-
-Label5=Form1:addLabel(11,3,"G")
-Label5.color=16777215
-Label5.W=1
-Label5.fontColor=0
-
-Label6=Form1:addLabel(2,3,"G")
-Label6.color=16777215
-Label6.W=1
-Label6.fontColor=0
-
-Label7=Form1:addLabel(4,59,"00-00(ТК)")
-Label7.W=9
-
---Параметры реактора
-Label8=Form1:addLabel(16,59,"Тгаз=000")
-Label8.W=8
-
-Label9=Form1:addLabel(27,59,"Nтк=000")
-Label9.W=7
-
-Label10=Form1:addLabel(37,59,"Тгр=0000")
-Label10.W=8
-
-Label11=Form1:addLabel(48,59,"Ртк=000")
-Label11.W=7
-
-Frame2=Form1:addFrame(63,2,1)
-Frame2.color=5197647
-Frame2.H=3
-Frame2.W=40
-
---Вывод реального времени
-Label12=Form1:addLabel(110,3,"time")
-Label12.W=4
-Label12.fontColor=65535
-
-Label13=Form1:addLabel(10,8,"ТК")
-Label13.W=2
-
-Label14=Form1:addLabel(13,8,"00-00")
-Label14.color=16777215
-Label14.W=5
-Label14.fontColor=255
+local w, h = gpu.getResolution()
 
 
-Form1onCreate(Form1)
-forms.run(Form1)
+gpu.setBackground(0x000000)
+gpu.setForeground(0xffffff)
+
+    --Режимы МТК
+    term.setCursor(8, 2)
+    term.write("Nтк")
+
+    term.setCursor(16, 2)
+    term.write("Tгр")
+
+    term.setCursor(24, 2)
+    term.write("Pтк")
+
+while true do
+    --Выводим время
+    local t = getTimeHost()
+    gpu.setBackground(0x404040)
+    gpu.setForeground(0xf5f5f5)
+    term.setCursor(w - 10, 2)
+    term.write(os.date("%H:%M:%S", t)) --Вывод времени
+    term.setCursor(w - 11, 3)
+    term.write(os.date("%d.%m.%Y", t)) --Вывод даты
+    term.setCursor(47, 30)
+    --Просто заглушка
+    term.write("МТК в режиме тестирования.")
+
+
+    --Завершение работы по нажатию
+if rsio.getInput(1) > 0 then
+    term.clear()
+    gpu.setBackground(0x000000)
+    gpu.setForeground(0xffffff)
+    computer.beep(800)
+    computer.beep(500)
+    computer.beep(300)
+    computer.beep(200)
+    term.setCursor(47, 30)
+    term.write("МТК Завершение работы...")
+    os.sleep(3)
+    computer.shutdown(true)
+end
+
+end
